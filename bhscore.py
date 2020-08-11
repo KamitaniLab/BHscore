a9c7bb19f1c26661c5cfc941ec3d12b839bf013e
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2020-08-11 15:05:08 +0900

    Update bhscore.py

--- bhscore.py
+++ bhscore.py
@@ -1,17 +1,36 @@
+'''A Module to compute the BH score.
+
+Author: Souma Nonaka, Shuntaro C. Aoki
+'''
+
+
 import os
 
 import numpy as np
 from scipy.stats import spearmanr, t
 
 
-def compute_bhscore(predacc_list, pval_threshold):
-    """
-    predacc_list: sequence of ndarray. Each ndarray represents prediction accuracies of DNN units from each ROI.  
-    Length of predacc_list should be the same as the number of layers. 
-    The shape of each ndarray should be [# of units x # of ROIs]. 
+def compute_bhscore(predacc_list, pval=0.05, return_top_rois=False):
+    """Compute a BH score of a given DNN.
+
+    Parameters
+    ----------
+    predacc_list : list of arrays
+        List of prediction accuracies for a DNN. Each array contains
+        prediction accuracies of individual units in a layer, formed as an
+         array of ROIs x units.
+    pval : float, default = 0.05
+        P-value threshold in unit selection.
+    return_top_rois : bool, default = False
+        Returns top ROIs if True.
+
+    Returns
+    -------
+    bhscore : float
+    top_rois: list of arrays
     """
 
-    best_rois = []
+    top_rois = []
     for predacc in predacc_list:
         # if prediction accuracy is nan, convert it to zero
         predacc[np.isnan(predacc)] = 0
@@ -31,37 +50,68 @@ def compute_bhscore(predacc_list, pval_threshold):
         select_unit_ind = np.logical_and(threshold, plus_unit)
         pred_max_ind = pred_max_ind[select_unit_ind]
 
-        best_rois.append(pred_max_ind)
+        top_rois.append(pred_max_ind)
 
     # get layer numbers of each unit. concatenate best ROIs for all layers
     layer_numbers = []
     best_roi_flatten = []
-    for i_br, br in enumerate(best_rois):
+    for i_br, br in enumerate(top_rois):
         layer_numbers.extend(np.repeat(i_br + 1, len(br)))
         best_roi_flatten.extend(br)
 
     # compute Spearman's rank correlation
     bhscore, _ = spearmanr(layer_numbers, best_roi_flatten)
 
-    return bhscore, best_rois
+    if return_top_rois:
+        return bhscore, top_rois
+    else:
+        return bhscore
 
 
-def random_sample_bhscore(predacc_list, pval_threshold, n_sample_layer=5, n_sample=100):
+def compute_bhscore_layerselect(predacc_list, pval=0.05, n_layers=5,
+                                n_repeat=100, return_top_rois=False):
+    """Compute a BH score of a given DNN, random layer selection version.
+
+    Parameters
+    ----------
+    predacc_list : list of arrays
+        List of prediction accuracies for a DNN. Each array contains
+        prediction accuracies of individual units in a layer, formed as an
+         array of ROIs x units.
+    pval : float, default = 0.05
+        P-value threshold in unit selection.
+    n_layers : int, default = 5
+        The number of layers used to compute the BH score. Note that the first
+        and last layers are always included in the computation. Thus,
+        (n_layers - 2) layers are randomly selected from the representative
+        layers except the first and last ones.
+    n_repeat : int, default = 100
+        The number of random layer selection.
+    return_top_rois : bool, default = False
+        Returns top ROIs if True.
+
+    Returns
+    -------
+    bhscore_list : arary of float
+    top_rois_list : list of list of arrays
     """
-    Randomly sample specified number of layers and compute mean BH score
-    """
 
-    bhscore_list = np.zeros(n_sample)
-    for i_s in range(n_sample):
+    bhscore_list = np.zeros(n_repeat)
+    top_rois_list = []
+    for i_s in range(n_repeat):
         # sample layers
-        sample_index = np.random.choice(np.arange(1, len(predacc_list)-1), size=n_sample_layer - 2, replace=False)
+        sample_index = np.random.choice(np.arange(1, len(predacc_list)-1), size=n_layers - 2, replace=False)
         sample_index = np.sort(sample_index)
         predacc_list_sampled = [predacc_list[0]] + [predacc_list[i] for i in sample_index] + [predacc_list[-1]]
 
-        bhscore, _ = compute_bhscore(predacc_list_sampled, pval_threshold)
+        bhscore, top_rois = compute_bhscore(predacc_list_sampled, pval, return_top_rois=True)
         bhscore_list[i_s] = bhscore
+        top_rois_list.append(top_rois)
 
-    return bhscore_list
+    if return_top_rois:
+        return bhscore_list, top_rois_list
+    else:
+        return bhscore_list
 
 
 if __name__ == '__main__':
