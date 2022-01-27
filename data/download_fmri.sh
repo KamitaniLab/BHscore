7bd9ea7f26e574fc4f6f2047ae16e419287e0429
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2022-01-27 15:56:05 +0900

    Update download_fmri.sh

--- data/download_fmri.sh
+++ data/download_fmri.sh
@@ -2,9 +2,9 @@
 
 set -Ceu
 
-[ -e ./fmri/sub-01_perceptionNaturalImageTraining_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-01_perceptionNaturalImageTraining_VC_v2.h5 https://ndownloader.figshare.com/files/24153026
-[ -e ./fmri/sub-02_perceptionNaturalImageTraining_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-02_perceptionNaturalImageTraining_VC_v2.h5 https://ndownloader.figshare.com/files/24153080
-[ -e ./fmri/sub-03_perceptionNaturalImageTraining_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-03_perceptionNaturalImageTraining_VC_v2.h5 https://ndownloader.figshare.com/files/24153098
-[ -e ./fmri/sub-01_perceptionNaturalImageTest_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-01_perceptionNaturalImageTest_VC_v2.h5 https://ndownloader.figshare.com/files/24153020
-[ -e ./fmri/sub-02_perceptionNaturalImageTest_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-02_perceptionNaturalImageTest_VC_v2.h5 https://ndownloader.figshare.com/files/24153077
-[ -e ./fmri/sub-03_perceptionNaturalImageTest_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-03_perceptionNaturalImageTest_VC_v2.h5 https://ndownloader.figshare.com/files/24153095
+[ -e ./fmri/sub-01_perceptionNaturalImageTest_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-01_perceptionNaturalImageTest_VC_v2.h5 https://ndownloader.figshare.com/files/33900890
+[ -e ./fmri/sub-01_perceptionNaturalImageTraining_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-01_perceptionNaturalImageTraining_VC_v2.h5 https://ndownloader.figshare.com/files/33900893
+[ -e ./fmri/sub-02_perceptionNaturalImageTest_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-02_perceptionNaturalImageTest_VC_v2.h5 https://ndownloader.figshare.com/files/33900896
+[ -e ./fmri/sub-02_perceptionNaturalImageTraining_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-02_perceptionNaturalImageTraining_VC_v2.h5 https://ndownloader.figshare.com/files/33900899
+[ -e ./fmri/sub-03_perceptionNaturalImageTest_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-03_perceptionNaturalImageTest_VC_v2.h5 https://ndownloader.figshare.com/files/33900902
+[ -e ./fmri/sub-03_perceptionNaturalImageTraining_VC_v2.h5 ] || wget --no-check-certificate -O ./fmri/sub-03_perceptionNaturalImageTraining_VC_v2.h5 https://ndownloader.figshare.com/files/33900908
